4cc83cefd6cf2770fe14146e139644ff4b60167e
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-05-04 09:33:17 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--seletor de caracteres--
+letra = input("digite a letra")
+if letra = "A":
+ print("você escolheu a letra A")
+elif letra == "B":
+  print("você escolheu a letra B")
+else:
+  print("essa opção não está disponível")
