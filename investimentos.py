06d643390677bc9d46d430598b42111c650f91ed
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-05-18 16:04:10 +0000

    v2

---
+++ investimentos.py
@@ -0,0 +1,10 @@
+#simulador de investimentos - poupança--
+deposito = float(input("digite o valor do aporte"))
+taxa = float(input("qual o valor da poupança em % ?"))
+meses = float(input("quantos meses no investimentos?"))
+conversao = taxa/100
+total = 0
+for mes in range(1, meses +1):
+    total = total + deposito
+    total = total +(total * taxa)
+print(f"ao final do periodo, você tera: R${total:2f}")
