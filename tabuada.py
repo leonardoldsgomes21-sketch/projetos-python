5ac6fd837d57aa5fcfd6ebce7b2962f48ed02a10
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-05-18 15:37:30 +0000

    v2

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,5 @@
 numero = int(input("digite a tabuada desejada: "))
 print(f"--tabuada do {numero}--")
-for i in range(1,11):
+for i in range(1, 11):
     resultado = numero * i
     print(f"{numero} x {i} = {resultado}")
