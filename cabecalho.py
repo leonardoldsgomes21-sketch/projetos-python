e3201ffd9cc6b5bb10076db624aabbf2657ae214
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-05-25 12:32:49 +0000

    v2

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+###definido e chamada a fnção
+def exibir_cabeçalho():
+    print('=' * 30)
+    print('sistema de gestão escolar')
+    print('versão 1.0 - desenvolvido no codespaces')
+    print('=' * 30)
+
+###chamado a função
+exibir_cabeçalho()
+print('menu principal:')
