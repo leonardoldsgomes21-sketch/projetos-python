4699bc6947c4f103cb2be44b7b09c3b259303e79
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-06-22 14:11:12 +0000

    v2

---
+++ podio.py
@@ -0,0 +1,7 @@
+#lista com os vencedores
+vencedores = ["brayan", "breno", "ravy"]
+
+#exibindo o podio
+print("🥇ouro:",  vencedores[0])
+print("🥈prata:", vencedores[1])
+print("🥉broze:", vencedores[2])
