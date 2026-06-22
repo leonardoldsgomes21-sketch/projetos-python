859d74235834ffbae93cbb1b81142e396f23d12b
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-06-22 12:40:36 +0000

    v2

---
+++ playlist.py
@@ -0,0 +1,6 @@
+print("music player")
+playlist = ["music 1-rock", "music 2-pop", "music 3-rap"]
+atual = 0
+print(f"tocando agora: {playlist[atual]}")
+print("musica anterior:--(inicio da playlist)")
+print("=" *40)
