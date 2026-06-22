a64983863013cbea6feccf0fef39fe62c4314cc3
leonardoldsgomes21-sketch <leonardo.lds.gomes21@gmail.com>
2026-06-22 12:53:36 +0000

    v2

--- playlist.py
+++ playlist.py
@@ -1,6 +1,7 @@
 print("music player")
 playlist = ["music 1-rock", "music 2-pop", "music 3-rap"]
 atual = int(input("digite o numero da musica"))
-print(f"tocando agora: {playlist[atual]}")
+indice = atual -1
+print(f"tocando agora: {playlist[indice]}")
 print("musica anterior:--(inicio da playlist)")
 print("=" *40)
